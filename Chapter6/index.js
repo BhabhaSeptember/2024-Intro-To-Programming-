912f71db6ae72937fce80dcd4ef7c002ebf52ed9
BhabhaSeptember <septemberbhabha@gmail.com>
2024-07-02 15:40:00 +0200

    first commit

---
+++ Chapter6/index.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// let message = "Hello 2024 World {^_^}!";
+// console.log(message);
+
+//CONDITIONALS
+// let myName = "Bhabha September";
+// let myName = "Bhabha";
+
+// console.log("Hello " + myName + "!");
+// if (myName.length > 7) {
+//     console.log("Wowzers, you have a REALLY long name!");
+// } else {
+//     console.log("Nice, your name isn't very long...");
+// };
+
+//CHANGING IF...ELSE STATEMENTS (think about SWITCH statements)
+// let lemonChicken = false;
+// let beefWithBlackBean = false;
+// let sweetAndSourPork = false;
+
+// if (lemonChicken) {
+//   console.log("Sweet! I'm having lemon chicken!");
+// } else if (beefWithBlackBean) {
+//   console.log("I'm having the beef.");
+// } else if (sweetAndSourPork) {
+//   console.log("Yay, I'll have the pork.");
+// } else {
+//   console.log("Well, I suppose I'll just have rice then.");
+// }
+
+//TRY IT OUT
+// let yourName = prompt("Please enter your name: ");
+
+// if (yourName === "Bhabha") {
+//   console.log("Welcome back Sister!");
+// } else if (yourName === "Audrey") {
+//   console.log("Hey Mom!");
+// } else if (yourName === "Victor") {
+//   console.log("Hi father!");
+// } else {
+//   console.log("Access denied!");
+// };
+
+//LOOPS
+//WHILE LOOP
+// let sheepCounted = 0;
+// while (sheepCounted < 10) {
+// console.log("I have counted " + sheepCounted + " sheep!");
+//  sheepCounted++;
+// }
+// console.log("Zzzzzzzzzzz");
+
+//FOR LOOP
+// for (let sheepCounted = 0; sheepCounted < 10; sheepCounted++) {
+//   console.log("I have counted " + sheepCounted + " sheep!");
+// }
+// console.log("Zzzzzzzzzzz");
+
+// let timesToSayHello = 7;
+// for (let i = 0; i < timesToSayHello; i++) {
+//   console.log(i + ". Hello!");
+// }
+
+//USING FOR LOOPS WITH ARRAYS && STRINGS
+// let animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
+// for (let i = 0; i < animals.length; i++) {
+//  console.log("This zoo contains a " + animals[i] + ".");
+// }
+
+// let myName = "Bhabha";
+
+// for(let i = 0; i < myName.length; i++) {
+//     console.log("My name contains the letter " + myName[i] + ".");
+// };
+
+for (let x = 2; x < 10000; x = x * 2) {
+    console.log(x);
+   };
+   
+   
